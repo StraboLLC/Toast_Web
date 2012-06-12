be55330022c7d6f4daab572ad05d737624aa1a83
Will Potter <will@wpotter.me>
2012-06-12 18:28:19 -0400

    lesssgo

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,12 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Toast::Application.initialize!
+
 require "aws/s3"
 con = AWS::S3::Base.establish_connection!(
   :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
 )
 aws_bucket = ENV['AMAZON_TOAST_BUCKET']
-
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Toast::Application.initialize!
