30472ad4fc3a9420665ec75012bb8bf6ae336477
Will Potter <will@wpotter.me>
2012-06-12 18:03:58 -0400

    FUCK

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-require 'active_support/dependencies'
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,6 @@
-AWS::S3::Base.establish_connection!(
+con = AWS::S3::Base.establish_connection!(
   :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
 )
 aws_bucket = ENV['AMAZON_TOAST_BUCKET']
+puts "** AMAZON CON='#{con.inspect}'"
