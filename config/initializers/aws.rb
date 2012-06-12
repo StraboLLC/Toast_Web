2137b0cbae7c3b3fee88097c55ff15185026648b
Will Potter <will@wpotter.me>
2012-06-12 18:53:06 -0400

    THIS SHIT WORKS

---
+++ config/initializers/aws.rb
@@ -0,0 +1,6 @@
+require "aws/s3"
+con = AWS::S3::Base.establish_connection!(
+  :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+)
+aws_bucket = ENV['AMAZON_TOAST_BUCKET']
