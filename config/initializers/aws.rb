22a266c6dcb6a4a63c1548ff7316bc31d1b475e0
Will Potter <will@wpotter.me>
2012-06-12 18:08:13 -0400

    Wtf

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,5 @@
+puts "\n\n\n\n\n\n\n"
+puts "Loading AMAZON..."
 con = AWS::S3::Base.establish_connection!(
   :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
